90fb01082bd9df6532f94bc90c91c5e3e9a8a3d0
dak98 <jakub.czajka1998@gmail.com>
2019-09-18 20:57:17 +0200

    Created the base layout of the server program.

--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,7 @@
 #include <QApplication>
+#include <QHBoxLayout>
+#include <QScopedPointer>
+#include <QTextEdit>
 #include <QWidget>
 
 int main(int argc, char *argv[])
@@ -8,8 +11,22 @@ int main(int argc, char *argv[])
     QWidget main_window;
     main_window.setWindowTitle("Server");
 
-    QTextEdit
+    QHBoxLayout main_layout;
+
+    QTextEdit messages_from_clients, user_console;
+    messages_from_clients.setReadOnly(true);
+
+    /*
+     * QWidget object takes ownership of the QHBoxLayout object which takes
+     * ownership of the QTextEdit objects.
+     */
+    main_layout.addWidget(&messages_from_clients);
+    main_layout.addWidget(&user_console);
+    main_window.setLayout(&main_layout);
 
     main_window.show();
+
+
+
     return a.exec();
 }
