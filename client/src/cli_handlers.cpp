6b06083fc5fd30cbfc348fcd117cfe81ef4410e9
dak98 <jakub.czajka1998@gmail.com>
2019-10-01 17:06:58 +0200

    Added option's name to the data being send between programs

--- client/src/cli_handlers.cpp
+++ client/src/cli_handlers.cpp
@@ -3,11 +3,26 @@
 namespace talk_net
 {
 
+auto cli_ECHO(QString arguments, socket_io::client& client_handle) -> QString
+{
+    QString const id = QString::number(client_handle.get_id());
+    try
+    {
+        client_handle.send("ECHO " + id.toStdString() + " " +
+                           arguments.toStdString());
+    }
+    catch (std::exception const& e)
+    {
+        return "An error occured: " + QString(e.what());
+    }
+    return QString();
+}
+
 auto cli_SEND(QString arguments, socket_io::client& client_handle) -> QString
 {
     try
     {
-        client_handle.send(arguments.toStdString());
+        client_handle.send("SEND " + arguments.toStdString());
     }
     catch (std::exception const& e)
     {
--- server/src/cli_handlers.cpp
+++ server/src/cli_handlers.cpp
@@ -3,7 +3,12 @@
 namespace talk_net
 {
 
-QString cli_LIST(QString arguments, socket_io::server& server_handle)
+auto cli_ECHO(QString arguments, socket_io::server& server_handle) -> QString
+{
+    return cli_SEND(arguments, server_handle);
+}
+
+auto cli_LIST(QString, socket_io::server& server_handle) -> QString
 {
     auto connected_clients = server_handle.get_connected_clients();
     QString output;
@@ -11,6 +16,31 @@ QString cli_LIST(QString arguments, socket_io::server& server_handle)
         output += QString::number(id) + ": " +
                   QString::fromStdString(client_view.to_string()) + "\n";
     return output;
+}
+
+auto cli_SEND(QString arguments, socket_io::server& server_handle) -> QString
+{
+    int const message_start_pos = arguments.indexOf(' ');
+    if (message_start_pos == -1)
+        return "Not enough arguments. Should be SEND [client's id] [message]";
+
+    QString const id_str = arguments.mid(0, message_start_pos);
+
+    bool result;
+    int const id = id_str.toInt(&result);
+    if (!id)
+        return "Client's id must be an integer";
+
+    arguments = arguments.mid(message_start_pos + 1, -1);
+    try
+    {
+        server_handle.send(id, "SEND " + arguments.toStdString());
+    }
+    catch (std::out_of_range const&)
+    {
+        return "Client with id " + id_str + " does not exist.";
+    }
+    return QString();
 }
 
 }
